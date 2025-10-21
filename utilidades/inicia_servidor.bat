c15a68a92d644cf8b12a9b9b64c3ba54cb24f0b3
DPHACA\LGC005 <carloslopezgonzalez@juntadeandalucia.es>
2025-10-21 17:27:15 +0200

    Modifica los bat a la nueva estructura de directorios

--- utilidades/inicia_servidor.bat
+++ utilidades/inicia_servidor.bat
@@ -1,5 +1,5 @@
 @echo off
 set HSQL_PATH=W:\BDDATLIBRE\hslqdb
-set DB_PATH=W:\BDDATLIBRE\BD\bdat
+set DB_PATH=W:\BDDATLIBRE\bdat
 java -cp "%HSQL_PATH%\hsqldb-jdk8.jar" org.hsqldb.server.Server --database.0 file:%DB_PATH%\bdat --dbname.0 bdat --port 9001
 pause
--- utilidades/inicia_servidor_y_devel.bat
+++ utilidades/inicia_servidor_y_devel.bat
@@ -1,5 +1,13 @@
 @echo off
 set HSQL_PATH=W:\BDDATLIBRE\hslqdb
-set DB_PATH=W:\BDDATLIBRE\BD
-java -cp "%HSQL_PATH%\hsqldb-jdk8.jar" org.hsqldb.server.Server --database.0 file:%DB_PATH%\bdat --dbname.0 bdat --database.1 file:%DB_PATH%\bdatdevel --dbname.1 bdatdevel --port 9001
+set DB_PRODUCCION=W:\BDDATLIBRE\bdat
+set DB_DESARROLLO=W:\BDDATLIBRE\desarrollo\bdat
+
+java -cp "%HSQL_PATH%\hsqldb-jdk8.jar" org.hsqldb.server.Server ^
+  --database.0 file:%DB_PRODUCCION%\bdat ^
+  --dbname.0 bdat ^
+  --database.1 file:%DB_DESARROLLO%\bdat ^
+  --dbname.1 bdat_desarrollo ^
+  --port 9001
+
 pause
